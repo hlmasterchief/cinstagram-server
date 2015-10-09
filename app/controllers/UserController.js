41c9e9bf4867f719799aa492ad447769d5466edc
HL <hlmasterchief93@gmail.com>
2015-10-09 18:54:07 +0300

    merge update + using async

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -2,6 +2,7 @@ var User = require('../models/User');
 var jwt = require('jsonwebtoken');
 var config = require('../config');
 var cloudinary = require('cloudinary');
+var async = require("async");
 
 var UserController = {
     signin: function(req, res, next) {
@@ -90,55 +91,114 @@ var UserController = {
     },
 
     update: function(req, res, next) {
-        if (req.body.oldPassword != req.authUser.password) {
-            res.send(403, {
-                success: false,
-                message: 'Wrong old password.'
-            });
-            return next();
-        }
+        if (!!req.body.password) {
+            if (req.body.oldPassword != req.authUser.password) {
+                res.send(403, {
+                    success: false,
+                    message: 'Wrong old password.'
+                });
+                return next();
+            }
 
-        req.authUser.email    = req.body.email,
-        req.authUser.username = req.body.username,
-        req.authUser.password = req.body.password;
+            req.authUser.password = req.body.password;
 
-        req.authUser.save(function(err) {
-            if (err) throw err;
-
-            res.send({
-                success: true,
-                message: 'Update success.',
-                username: req.authUser.username
-            });
-            next();
-        });
-    },
-
-    updateAvatar: function(req, res, next) {
-        if (!req.files.avatar) {
-            res.send({
-                success: false,
-                message: 'Avatar uploaded failed.'
-            });
-            return next();
-        }
-
-        cloudinary.uploader.upload(req.files.avatar.path, function(result) { 
-            
-            req.authUser.avatar = result.url;
             req.authUser.save(function(err) {
                 if (err) throw err;
 
                 res.send({
                     success: true,
-                    message: 'Update success.',
-                    username: req.authUser.avatar
+                    message: 'Update password success.',
+                    username: req.authUser.username
                 });
                 next();
             });
-        },
-        {
-            folder: "avatar"
+            return next();
+        }
+
+        if (!!req.files.avatar) {
+            cloudinary.uploader.upload(req.files.avatar.path, function(result) { 
+                
+                req.authUser.avatar = result.url;
+                req.authUser.save(function(err) {
+                    if (err) throw err;
+
+                    res.send({
+                        success: true,
+                        message: 'Update avatar success.',
+                        avatar: req.authUser.avatar
+                    });
+                    next();
+                });
+            }, {
+                folder: "avatar"
+            });
+            return next();
+        }
+
+        if (req.body.email.length === 0) {
+            res.send({
+                success: false,
+                message: 'Email has not been filled.'
+            });
+            return next();
+        }
+
+        if (req.body.username.length === 0) {
+            res.send({
+                success: false,
+                message: 'Username has not been filled.'
+            });
+            return next();
+        }
+
+        async.series([
+            function(callback) {
+                User.findOne({ email: req.body.email }, function (err, user) {
+                    if (err) throw err;
+
+                    if ((!!user) && (!user._id.equals(req.authUser._id))) {
+                        res.send({
+                            success: false,
+                            message: 'Email has been used.'
+                        });
+                        return callback(new Error('Email has been used.'));
+                    }
+                    callback();
+                });
+            },
+            function(callback) {
+                User.findOne({ username: req.body.username }, function (err, user) {
+                    if (err) throw err;
+
+                    if ((!!user) && (!user._id.equals(req.authUser._id))) {
+                        res.send({
+                            success: false,
+                            message: 'Username has been used.'
+                        });
+                        return callback(new Error('Username has been used.'));
+                    }
+                    callback();
+                });
+            }
+        ], function(err) {
+            if (err) {
+                console.log("final" + err);
+                return next();
+            }
+            
+            req.authUser.email    = req.body.email,
+            req.authUser.username = req.body.username,
+
+            req.authUser.save(function(err) {
+                if (err) throw err;
+
+                res.send({
+                    success: true,
+                    message: 'Update username/email success.',
+                    username: req.authUser.username
+                });
+                next();
+            });
         });
     },
 
